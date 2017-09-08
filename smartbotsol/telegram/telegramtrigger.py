7a2b570fd4f1c155db9cc0216bb2459d39222c80
Mr D <dqunbp@gmail.com>
2017-09-08 20:00:16 +0300

    refactor send_msg and send keys

--- smartbotsol/telegram/telegramtrigger.py
+++ smartbotsol/telegram/telegramtrigger.py
@@ -1,23 +1,35 @@
 
 
-import telegram as tm
+import telegram
 import os
 import logging as log
 from smartbotsol.telegram.utils.helpers import send_action
 from telegram.chataction import ChatAction
 from smartbotsol.telegram.utils.helpers import transform_keyboard_to_inline
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
 
 from smartbotsol import BaseTrigger
 
 TYPING, UPLOAD_PHOTO = (ChatAction.TYPING, ChatAction.UPLOAD_PHOTO)
 
 class TelegramTrigger(BaseTrigger):
-    
+    """Wrap for telegram bot update"""
+
+
     def __init__(self):
         self.user = None
         self.bot = None
         self.update = None
+        
+        self.default_message_kwargs = {
+            'parse_mode': telegram.ParseMode.MARKDOWN,
+            'disable_web_page_preview': True,
+            'disable_notification': True
+        }
+        self.default_markup_kwargs = {
+            'resize_keyboard': True,
+            'one_time_keyboard': False,
+        }
 
     def __str__(self):
         return str((self.user, self.bot, self.update))
@@ -42,31 +54,56 @@ class TelegramTrigger(BaseTrigger):
     def get_venue(self):
         return self.update.message.venue if self.update.message.venue else None
 
+    def send_venue(self, title, address, location):
+        """Sends telegram location"""
+        assert isinstance(locals, list)
+        return self.bot.sendVenue(
+            chat_id=self.chat_id,
+            latitude=location[0],
+            longitude=location[1],
+            title=title,
+            address=address
+        )
 
-    @send_action(TYPING)
-    def send_msg(self, txt):
-        return self.bot.sendMessage(chat_id=self.chat_id,
-                             text=txt,
-                             disable_web_page_preview=True,
-                             parse_mode=tm.ParseMode.MARKDOWN)
+    def send_message(self, txt, remove_keyboard=False, **kwargs):
+        """Sends telegram message"""
+        message_kwargs = self.default_message_kwargs.copy()
+        message_kwargs.update(**kwargs)
+        if remove_keyboard:
+            message_kwargs.setdefault(
+                'reply_markup',
+                ReplyKeyboardRemove()
+            )
+        return self.bot.sendMessage(
+            chat_id=self.chat_id,
+            text=txt,
+            **message_kwargs
+        )
 
-    @send_action(TYPING)
-    def send_keys(self, txt, keyboard, inline = False):
-        reply_markup = tm.ReplyKeyboardMarkup(keyboard=keyboard,
-                                              resize_keyboard=True,
-                                              one_time_keyboard=True)
+    def send_keyboard(self, txt, keyboard, inline=False,**kwargs):
+        """Sends telegram message with keyboard"""
+        message_kwargs = self.default_message_kwargs.copy()
+        markup_kwargs = self.default_markup_kwargs.copy()
+        message_kwargs.update(**kwargs)
+        markup_kwargs.update(
+            {k: v for k,v in kwargs.items() if k in self.default_markup_kwargs}
+        )
+        reply_markup = telegram.ReplyKeyboardMarkup(
+            keyboard=keyboard,
+            **markup_kwargs
+        )
         if inline:
-            inline_keyboard = transform_keyboard_to_inline(keyboard)
-            log.debug(keyboard)
-            reply_markup = InlineKeyboardMarkup(inline_keyboard)
+            reply_markup = InlineKeyboardMarkup(
+                transform_keyboard_to_inline(keyboard),
+                **markup_kwargs
+            )
+        return self.bot.sendMessage(
+            chat_id=self.chat_id,
+            text=txt,
+            reply_markup=reply_markup,
+            **message_kwargs
+        )
 
-        return self.bot.sendMessage(chat_id=self.chat_id,
-                             text=txt,
-                             disable_web_page_preview=True,
-                             parse_mode=tm.ParseMode.MARKDOWN,
-                             reply_markup=reply_markup)
-
-    @send_action(UPLOAD_PHOTO)
     def send_photo(self, src):
         return self.bot.sendPhoto(chat_id=self.chat_id, photo=src)
 
